4817902fe9f08865e9fbc1389ccd1241f2f4ef21
Motwani <Deepika.Motwani@ap.jll.com>
2023-10-27 16:42:45 +0530

    removed n modified unnecessary code

--- src/components/Button/ArrowBlack.tsx
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import { ReactComponent as ArrowBlack } from '../../assets/btn_arrow_black.svg';
-
-// export const ArrowBlackIcon = () => {
-//   return <ArrowBlack />;
-// };
